93a4d2ab4f84e6d92bde1b926914342c952f1a5e
ke-sasada <sasada.k.ab@m.titech.ac.jp>
2016-05-27 16:46:15 +0900

    演習:Add swipe motion test

--- app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/gles10ex/MainActivity.java
@@ -1,18 +1,30 @@
 package jp.ac.titech.itpro.sdl.gles10ex;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.widget.SeekBar;
 
 
-public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener,SensorEventListener ,GestureDetector.OnGestureListener{
     private final static String TAG = "MainActivity";
 
     private GLSurfaceView glView;
     private SimpleRenderer renderer;
     private SeekBar rotationBarX, rotationBarY, rotationBarZ;
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    private GestureDetector gestureDetector;
+    private float rotX = 0, rotY = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +45,11 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
       //  renderer.addObj(new Pyramid(0.5f, 0, 0, 0));
         renderer.addObj(new TriPrism(0.5f,0,0,0));
         glView.setRenderer(renderer);
+
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        gestureDetector = new GestureDetector(this,this);
     }
 
     @Override
@@ -40,6 +57,8 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         super.onResume();
         Log.d(TAG, "onResume");
         glView.onResume();
+
+        sensorManager.registerListener(this,sensor,SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     @Override
@@ -47,6 +66,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         super.onPause();
         Log.d(TAG, "onPause");
         glView.onPause();
+
+        sensorManager.unregisterListener(this);
+
     }
 
     @Override
@@ -65,6 +87,64 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+         //   renderer.setRotationX(sensorEvent.values[0]*2);
+         //   renderer.setRotationY(sensorEvent.values[1]*2);
+         //   renderer.setRotationZ(sensorEvent.values[2]*2);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        Log.d(TAG,"SCROOL");
+
+        rotX -= (v1);
+        rotY -= (v);
+
+        renderer.setRotationX(rotX);
+        renderer.setRotationY(rotY);
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        this.gestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 
 }
